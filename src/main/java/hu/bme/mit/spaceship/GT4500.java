0de2b923002db9b09cf642b17048f744d70ca83a
Szacsi5104 <benceszauter@gmail.com>
2021-04-13 15:22:08 +0200

    rand_valtozo

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -83,6 +83,7 @@ public class GT4500 implements SpaceShip {
         }
         //TODO implement feature
         else{
+          boolean rand=false;
           firingSuccess=false;
         }
         break;
